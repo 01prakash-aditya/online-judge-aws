7318607093a5db582b15619843378aa20e70b65f
01prakash-aditya <aditya_2302cs11@iitp.ac.in>
2025-06-09 09:12:06 +0530

    create

---
+++ client/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+const isDocker = process.env.IS_DOCKER === 'true'
+const apiTarget = isDocker ? 'http://api:3000' : 'http://localhost:3000'
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ['@reduxjs/toolkit'],
+  },
+  server: {
+    host: true, // or '0.0.0.0'
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: apiTarget,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+})
